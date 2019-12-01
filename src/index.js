391897b1edcb8855956db04fb8963d7063b62c4f
n1lux <n1lux.comp@gmail.com>
2019-12-01 15:46:09 -0300

    Adding Component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from './App';
+import './index.css';
 
-class HelloMessage extends React.Component {
-    render() {
-        return <div>
-            <div className="container">
-                <h1>Hello {this.props.name}</h1>
-            </div>
-        </div>
-    }
-}
-
-let App = document.getElementById("app");
-
-ReactDOM.render(<HelloMessage name="Hello word!" />, App);
+ReactDOM.render(<App />, document.getElementById('root'));
