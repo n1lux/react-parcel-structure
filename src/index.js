db519c60688f2a52109a3cb799659ddb39e85860
n1lux <n1lux.comp@gmail.com>
2019-12-01 14:59:58 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class HelloMessage extends React.Component {
+    render() {
+        return <div>
+            <div className="container">
+                <h1>Hello {this.props.name}</h1>
+            </div>
+        </div>
+    }
+}
+
+let App = document.getElementById("app");
+
+ReactDOM.render(<HelloMessage name="Hello word!" />, App);
